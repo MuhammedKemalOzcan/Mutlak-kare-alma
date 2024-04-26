f38a24688d2d4fc598e45a013eecf8dd651432d1
mami_ <mami_@DESKTOP-H9BT6T7>
2024-04-26 15:52:40 +0300

    Add project files.

---
+++ Mutlak kare alma/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Mutlak_kare_alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aralarında boşluk bırakarak sayı giriniz:");
+            var sayilar = Console.ReadLine();
+
+            var parts = sayilar.Split(' ');
+
+            int result = 0;
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (Convert.ToInt32(parts[i]) < 67)
+                {
+                    result += 67 - Convert.ToInt32(parts[i]);
+                }
+                else
+                {
+                    result += (67 - Convert.ToInt32(parts[i])) * -1;
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
